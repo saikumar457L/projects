ceb127b297f6622d085d17d424f663a8d6cbd8e8
saikumar457L <saikumarlovely5@gmail.com>
2020-05-24 20:54:38 +0530

    static files are added to mysite

--- mysite_project/mysite/polls/admin.py
+++ mysite_project/mysite/polls/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from .models import Question , Choice
 
 # Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text","pub_date"]
+    inlines = [ChoiceInline]
 
-    
+
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ["question","choice_text","votes"]
